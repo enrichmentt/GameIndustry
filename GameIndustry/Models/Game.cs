061d0b542d84870770911b77831a15104403813f
Vladimir P <enrichment.off@gmail.com>
2021-04-13 21:17:25 +0300

    Добавлена модель

---
+++ GameIndustry/Models/Game.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GameIndustry.Models
+{
+    /// <summary>
+    /// Модель игры
+    /// </summary>
+    public class Game
+    {
+        public string Name { get; set; }
+        public string Genre { get; set; }
+        public DateTime ReleaseDte { get; set; }
+    }
+}
